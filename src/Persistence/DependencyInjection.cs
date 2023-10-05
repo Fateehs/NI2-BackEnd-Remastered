0ca84f3bac10fec1ae37f953d94543488200e9f0
Fateehs <benzer1010@gmail.com>
2023-10-05 16:06:25 +0300

    data: dbcontext file created and set, dependency injection file created and set, namespace of persistence changed to data

---
+++ src/Persistence/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NI2.Data;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgreSQL");
+
+        return services;
+    }
+}
---
+++ src/Persistence/NIDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NI2.Data;
+
+public class NIDbContext : DbContext
+{
+    public NIDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(NIDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
